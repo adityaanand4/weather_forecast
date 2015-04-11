7af2bb0e3fc3790f0ca7b2878ac1d1d0c394ad81
Aditya <adityaanand4@gmail.com>
2015-04-12 02:24:17 +0530

    assignment complete!

--- routes/users.js
+++ routes/users.js
@@ -19,19 +19,33 @@ router.post('/', function (req, res, next) {
 
                 if (!err) {
 
-                    var body= JSON.parse(forecastRecieved.body)
+                    var body = JSON.parse(forecastRecieved.body)
 
                     //If Api result do not contain a error code
                     if (body.cod === "200") {
 
-                        var date=new Date()
+                        //Remove unnecessary data
+                        for (var i = 0; i < body.cnt; i++) {
 
+                            //Creating UTC time from epoch time
+                            body.list[i].dt = new Date(body.list[i].dt * 1000 );
 
+                            for (var j = 0; j < body.list[i].weather.length; j++) {
+                                delete body.list[i].weather[j].icon;
+                            }
+
+                        }
+                        delete body.cod
+                        delete body.message
+                        delete body.cnt
+                        delete body.city.population
+                        delete body.city.sys
+
+                        //push the final data
                         forecast.push(body)
-                    }else
-                    {
+                    } else {
                         //Generate a error for that city
-                        var errObj="Sorry,Something went wrong for "+reqJson.cities[i];
+                        var errObj = "Sorry,Something went wrong for " + reqJson.cities[i];
                         forecast.push(errObj);
                     }
 
